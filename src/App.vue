70859d9b5fefbfe681cbe541f180df5635fb45ba
Jesús <jesusrugarciasandia@gmail.com>
2025-07-30 13:57:13 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SideBar from './components/SideBar.vue'
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+  <SideBar />
+</template>
+
+<style scoped>
+
+</style>
